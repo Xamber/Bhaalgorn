0102f5db0de3525a445bedc009daeac6c02b4713
Artem Filippov <Artem.Filippov@emc.com>
2018-06-27 12:19:51 +0300

    Linear Regression with mul variable

--- exc.py
+++
@@ -1,2 +0,0 @@
-class TrainingSetsException(Exception):
-    pass
--- linear.py
+++ linear.py
@@ -1,61 +1,64 @@
-from exc import TrainingSetsException
+import random
 
 
-class Example:
-
-    def __init__(self, input, output):
-        if len(input) != len(output):
-            raise TrainingSetsException("Len of inputs and outputs are not a same")
-
-        self.input = input
-        self.output = output
+class TrainingSetsException(Exception):  pass
 
 
 class LinearRegression:
 
-    def __init__(self, dataset):
-        self.weight_0 = 0
-        self.weight_1 = 0.5
-        self.dataset = dataset
-        self.lenght_dataset = len(self.dataset.input)
+    def __init__(self, features_count):
 
+        self.inputs = []
+        self.outputs = []
+
+        self.features_count = features_count
+        self.weights = [random.uniform(0, 1) for _ in range(self.features_count+1)]
         self._epoch = 1
 
-    def hypothesis(self, x):
-        return self.weight_0 + self.weight_1 * x
+    @property
+    def lenght_dataset(self):
+        return len(self.inputs)
+
+    def add_example(self, input_vector, output):
+
+        if not len(input_vector):
+            raise TrainingSetsException("Cannot add zero len vector")
+
+        if len(input_vector) != self.features_count:
+            raise TrainingSetsException("Len of vector and len of features count are not a same")
+
+        self.inputs.append(input_vector)
+        self.outputs.append(output)
+
+    def hypothesis(self, vector):
+        return self.weights[0] + sum([x * w for x, w in zip(vector, self.weights[1:])])
 
     def error(self):
-        predicted_set = [self.hypothesis(x) for x in self.dataset.input]
-        output = self.dataset.output
-        return (1 / (self.lenght_dataset * 2)) * sum([pow(h - y, 2) for h, y in zip(predicted_set, output)])
+        predicted = [self.hypothesis(x) for x in self.inputs]
+        return (1 / (self.lenght_dataset * 2)) * sum([pow(h - y, 2) for h, y in zip(predicted, self.outputs)])
 
     def gradient(self, speed):
-        predicted_set = [self.hypothesis(x) for x in self.dataset.input]
-        output = self.dataset.output
-        input = self.dataset.input
-        new_weight_0 = self.weight_0 - speed * (1 / (self.lenght_dataset)) * sum(
-            [h - y for h, y in zip(predicted_set, output)])
-        new_weight_1 = self.weight_1 - speed * (1 / (self.lenght_dataset)) * sum(
-            [(h - y) * x for h, y, x in zip(predicted_set, output, input)])
-        self.weight_0 = new_weight_0
-        self.weight_1 = new_weight_1
+        new_weights = []
+        predicted = [self.hypothesis(x) for x in self.inputs]
+        for index, w in enumerate(self.weights):
+            new_weight = w - speed * (1 / self.lenght_dataset) * sum([(h - y) * (1 if index == 0 else x[index-1]) for h, y, x in zip(predicted, self.outputs, self.inputs)])
+            new_weights.append(new_weight)
+        self.weights = new_weights
         self._epoch += 1
 
+l = LinearRegression(1)
+l.add_example([1], 1)
+l.add_example([2], 2)
+l.add_example([3], 3)
 
-dataset = Example([1, 2, 3, 4], [3, 4, 5, 6])
 
-l = LinearRegression(dataset)
 print(l.error())
 
 for i in range(0, 190):
-    l.gradient(0.1)
+    l.gradient(0.2)
 
-print(l.error())
-
-print("=========")
-print(l.weight_0)
-print(l.weight_1)
 
 print("=========")
+print(l.weights)
+print("=========")
 print(l.error())
-
