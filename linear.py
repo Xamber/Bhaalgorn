04abfcc42ecacc3f869fd215f3a1f60f832ea10f
Artem Filippov <Artem.Filippov@emc.com>
2018-06-28 20:07:49 +0300

    integrate numpy

--- linear.py
+++ linear.py
@@ -1,30 +1,28 @@
-import random
 import numpy as np
-
-class TrainingSetsException(Exception):  pass
-
-# y = np.array([10,10,10])
-# y.reshape(-1, 1)
-# x = np.array([(1,2,3), (4,5,6), (7,8,9)])
 
 
 class LinearRegression:
-
     def __init__(self, features_count, dataset):
 
         self.dataset = dataset
+        self.features_count = features_count
+
         self.inputs = self.dataset[..., :-1]
         self.outputs = self.dataset[..., -1:]
 
-        self.features_count = features_count
-        self.weights = np.ones(self.features_count+1)
+        self.weights = np.random.uniform(0, 1, self.features_count + 1)
+        #self.weights = np.array([0, 1])
         self._epoch = 1
 
         self._prepared_input_cache = None
 
     @property
-    def lenght_dataset(self):
+    def len_dataset(self):
         return len(self.inputs)
+
+    @property
+    def prepared_output(self):
+        return np.swapaxes(self.outputs, 0, 1)
 
     @property
     def prepared_input(self):
@@ -34,11 +32,12 @@ class LinearRegression:
         return self._prepared_input_cache
 
     def hypothesis(self, vector):
-        return np.dot(self.weights, np.concatenate([np.array([1]), vector])).sum()
+        return np.dot(self.weights, np.concatenate([np.ones(1), vector])).sum()
 
     def error(self):
+        squared_error = np.vectorize(lambda x, y: pow(x - y, 2))
         predicted = [self.hypothesis(x) for x in self.inputs]
-        error = (1 / (self.lenght_dataset * 2)) * sum([pow(h - y, 2) for h, y in zip(predicted, self.outputs)])
+        error = (1 / (self.len_dataset * 2)) * squared_error(predicted, self.prepared_output).sum()
         return np.asscalar(error)
 
     def accuracy(self):
@@ -46,17 +45,18 @@ class LinearRegression:
         return accuracy if accuracy >= 0 else 0
 
     def gradient(self, speed):
+        cost = np.vectorize(lambda h, y, x: (h - y) * x)
+
         new_weights = []
-        predicted = [self.hypothesis(x) for x in self.inputs]
+        predicted = np.array([self.hypothesis(x) for x in self.inputs])
         for index, weight in enumerate(self.weights):
-            new_weight = weight - speed * (1 / self.lenght_dataset) * sum([(h - y) * x for h, y, x in zip(predicted, self.outputs, self.prepared_input)])
-            new_weights.append(new_weight)
+            new_weights.append(weight - (speed * (1 / self.len_dataset) * cost(predicted, self.prepared_output, np.swapaxes(self.prepared_input, 0, 1)[index]).sum()))
 
         self.weights = new_weights
         self._epoch += 1
 
-    def train_gradient(self, timeout=100000):
-        speed = 0.01
+    def train_gradient(self, timeout=3000):
+        speed = 0.1
         iteration = 0
         latest_error = 1
 
@@ -66,35 +66,45 @@ class LinearRegression:
                 break
 
             if error > latest_error:
-                speed = speed * 0.9
-
-            #print(error, latest_error, error > latest_error, latest_error-error, speed)
-
+                speed = speed * 0.5
             self.gradient(speed)
 
             latest_error = error
             iteration += 1
 
 
+
 dataset = np.array([
-    [(1 - 2.5) / 5, (500 - 500) / 1000, (24000-50000)/100000],
-    [(1 - 2.5) / 5, (1000 - 500) / 1000, (22000-50000)/100000],
-    [(2 - 2.5) / 5, (500 - 500) / 1000, (32000-50000)/100000],
-    [(2 - 2.5) / 5, (1000 - 500) / 1000, (29000-50000)/100000],
-    [(3 - 2.5) / 5, (500 - 500) / 1000, (40000-50000)/100000],
-    [(1 - 2.5) / 5, (500 - 500) / 1000, (24000-50000)/100000],
-    [(1 - 2.5) / 5, (1000 - 500) / 1000, (22000-50000)/100000],
-    [(2 - 2.5) / 5, (500 - 500) / 1000, (32000-50000)/100000],
-    [(2 - 2.5) / 5, (1000 - 500) / 1000, (29000-50000)/100000],
-    [(3 - 2.5) / 5, (500 - 500) / 1000, (40000-50000)/100000],
+    [1, 1+10],
+    [2, 2+10],
+    [3, 3+10],
+    [4, 4+10],
+    [5, 5+10],
+    [6, 6+10],
+    [7, 7+10],
+    [8, 8+10],
+    [9, 9+10],
+])
+
+dataset = np.array([
+    [(1 - 2.5) / 5, (500 - 500) / 1000, (24000 - 50000) / 100000],
+    [(1 - 2.5) / 5, (1000 - 500) / 1000, (22000 - 50000) / 100000],
+    [(2 - 2.5) / 5, (500 - 500) / 1000, (32000 - 50000) / 100000],
+    [(2 - 2.5) / 5, (1000 - 500) / 1000, (29000 - 50000) / 100000],
+    [(3 - 2.5) / 5, (500 - 500) / 1000, (40000 - 50000) / 100000],
+    [(1 - 2.5) / 5, (500 - 500) / 1000, (24000 - 50000) / 100000],
+    [(1 - 2.5) / 5, (1000 - 500) / 1000, (22000 - 50000) / 100000],
+    [(2 - 2.5) / 5, (500 - 500) / 1000, (32000 - 50000) / 100000],
+    [(2 - 2.5) / 5, (1000 - 500) / 1000, (29000 - 50000) / 100000],
+    [(3 - 2.5) / 5, (500 - 500) / 1000, (40000 - 50000) / 100000],
 ])
 
 l = LinearRegression(2, dataset)
-print()
-tring = l.hypothesis([(1-2.5)/5, (1000-500)/1000])
+
 print(l.accuracy())
 print(l.prepared_input)
-l.train_gradient(10000)
+l.train_gradient(3000)
+
 print("_epoch: ", l._epoch)
 print("weights: ", l.weights)
 print("accuracy: ", l.accuracy())
