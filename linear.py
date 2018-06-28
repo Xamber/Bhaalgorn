cd001fda4bc019a0c5d7e8d780961e05c601790f
Artem Filippov <Artem.Filippov@emc.com>
2018-06-28 12:25:32 +0300

    Starting to use numpy

--- linear.py
+++ linear.py
@@ -1,18 +1,23 @@
 import random
-
+import numpy as np
 
 class TrainingSetsException(Exception):  pass
+
+# y = np.array([10,10,10])
+# y.reshape(-1, 1)
+# x = np.array([(1,2,3), (4,5,6), (7,8,9)])
 
 
 class LinearRegression:
 
-    def __init__(self, features_count):
+    def __init__(self, features_count, dataset):
 
-        self.inputs = []
-        self.outputs = []
+        self.dataset = dataset
+        self.inputs = self.dataset[..., :-1]
+        self.outputs = self.dataset[..., -1:]
 
         self.features_count = features_count
-        self.weights = [random.uniform(0, 1) for _ in range(self.features_count+1)]
+        self.weights = np.ones(self.features_count+1)
         self._epoch = 1
 
         self._prepared_input_cache = None
@@ -35,8 +40,8 @@ class LinearRegression:
         if len(input_vector) != self.features_count:
             raise TrainingSetsException("Len of vector and len of features count are not a same")
 
-        self.inputs.append(input_vector)
-        self.outputs.append(output)
+        self.inputs = np.vstack([self.inputs, np.array(input_vector)])
+        self.outputs = np.vstack([self.outputs, np.array([output])])
         self._prepared_input_cache = None
 
     def hypothesis(self, vector):
@@ -73,7 +78,7 @@ class LinearRegression:
             if error > latest_error:
                 speed = speed * 0.9
 
-            print(error, latest_error, error > latest_error, latest_error-error, speed)
+            #print(error, latest_error, error > latest_error, latest_error-error, speed)
 
             self.gradient(speed)
 
@@ -81,22 +86,25 @@ class LinearRegression:
             iteration += 1
 
 
-l = LinearRegression(2)
-l.add_example([(1-2.5)/5, (500-500)/1000], (24000-50000)/100000)
-l.add_example([(1-2.5)/5, (1000-500)/1000], (22000-50000)/100000)
-l.add_example([(2-2.5)/5, (500-500)/1000], (32000-50000)/100000)
-l.add_example([(2-2.5)/5, (1000-500)/1000], (29000-50000)/100000)
-l.add_example([(3-2.5)/5, (500-500)/1000], (40000-50000)/100000)
-l.add_example([(1-2.5)/5, (500-500)/1000], (24000-50000)/100000)
-l.add_example([(1-2.5)/5, (1000-500)/1000], (22000-50000)/100000)
-l.add_example([(2-2.5)/5, (500-500)/1000], (32000-50000)/100000)
-l.add_example([(2-2.5)/5, (1000-500)/1000], (29000-50000)/100000)
-l.add_example([(3-2.5)/5, (500-500)/1000], (40000-50000)/100000)
+dataset = np.array([
+    [(1 - 2.5) / 5, (500 - 500) / 1000, (24000-50000)/100000],
+    [(1 - 2.5) / 5, (1000 - 500) / 1000, (22000-50000)/100000],
+    [(2 - 2.5) / 5, (500 - 500) / 1000, (32000-50000)/100000],
+    [(2 - 2.5) / 5, (1000 - 500) / 1000, (29000-50000)/100000],
+    [(3 - 2.5) / 5, (500 - 500) / 1000, (40000-50000)/100000],
+    [(1 - 2.5) / 5, (500 - 500) / 1000, (24000-50000)/100000],
+    [(1 - 2.5) / 5, (1000 - 500) / 1000, (22000-50000)/100000],
+    [(2 - 2.5) / 5, (500 - 500) / 1000, (32000-50000)/100000],
+    [(2 - 2.5) / 5, (1000 - 500) / 1000, (29000-50000)/100000],
+    [(3 - 2.5) / 5, (500 - 500) / 1000, (40000-50000)/100000],
+])
+
+l = LinearRegression(2, dataset)
 
 
 print(l.accuracy())
 print(l.prepared_input)
-l.train_gradient()
+l.train_gradient(1000)
 print("_epoch: ", l._epoch)
 print("weights: ", l.weights)
 print("accuracy: ", l.accuracy())
