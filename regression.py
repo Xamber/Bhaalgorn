dfa222c113baa411a224cc5b3083da40f32ac83f
Artem Filippov <Artem.Filippov@emc.com>
2018-07-04 16:48:08 +0300

    - BaseRegression

--- regression.py
+++ regression.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class LinearRegression:
+class BaseRegression:
 
     def __init__(self, dataset):
         self.dataset = dataset
@@ -19,29 +19,34 @@ class LinearRegression:
         # Epoch of training.
         self.epoch = 1
 
-        # Some prepared matrix/vectors for future usage.
+        # Some prepared matrix/vectors for future usage:
         # - Matrix of input data with 1 as a first value: Use in prediction of training inputs and gradient.
         self.input_vectors = np.concatenate([np.ones((self.len_dataset, 1)), self.inputs], axis=1)
-        # - Flatten output: use in error.
-        self.output_vector = self.outputs.flatten()
-
         # - Tranposed input vectors: used in gradient and normal equation
         self.trans_input = np.transpose(self.input_vectors)
+        # - Flatten output: use in error.
+        self.output_vector = self.outputs.flatten()
         # - Clone output vector to matrix with len(weights) rows
         self.cloned_output = np.tile(self.output_vector, (len(self.weights), 1))
 
-    def show_info(self, show_predicted_vs_putput=False):
-        predicted_vs_putput = ""
-        if show_predicted_vs_putput:
-            predicted_vs_putput = [f"{p} - {o}\n" for p, o in zip(self.predicted, self.output_vector)]
+    def hypothesis(self, vector):
+        raise NotImplemented
 
-        print(f"========== {self.__class__.__name__} ========= \n"
-              f"Count of Features: {self.features_count} | Len of Dataset: {self.len_dataset} | Epoch: {self.epoch} \n"
-              f"Weights: {self.weights}\n"
-              f"Accuracy: {round(self.accuracy, 6)}% | Error: {self.error}\n"
-              f"{''.join(predicted_vs_putput)}"
-              f"=====================================\n"
-              )
+    def calculate(self, vector):
+        # proxy method for hypothesis.
+        return self.hypothesis(vector)
+
+    @property
+    def error(self):
+        raise NotImplemented
+
+    def normal_equation(self):
+        raise NotImplemented
+
+    @property
+    def accuracy(self):
+        # Calculate accuracy based on error function.
+        return round(100.00 - self.error * 100.00, 4)
 
     @property
     def predicted(self):
@@ -53,34 +58,11 @@ class LinearRegression:
         # Clone predicted vector to matrix with len(weights) rows
         return np.tile(self.predicted, (len(self.weights), 1))
 
-    @property
-    def error(self):
-        # Calculate squared error of training set. Scalar value.
-        return (1 / (self.len_dataset * 2)) * np.power(self.predicted - self.output_vector, 2).sum()
-
-    @property
-    def accuracy(self):
-        # Calculate accuracy based on error function.
-        return 100.00 - self.error * 100.00
-
-    def hypothesis(self, vector):
-        # Calculate value of input vector based on current weight
-        return np.dot(self.weights, np.concatenate([np.ones(1), vector])).sum()
-
-    def calculate(self, vector):
-        # proxy method for hypothesis.
-        return self.hypothesis(vector)
-
     def gradient(self, speed):
         # Gradient Descent iteration function.
         permit = speed / self.len_dataset
         self.weights -= ((self.cloned_predicted - self.cloned_output) * self.trans_input).sum(axis=1) * permit
         self.epoch += 1
-
-    def normal_equation(self):
-        # Normal Equation method
-        theta = np.linalg.inv(self.trans_input.dot(self.input_vectors)).dot(self.trans_input).dot(self.outputs)
-        self.weights = theta.flatten()
 
     def train_gradient(self, timeout=3000):
         # Gradient Descent method with automation decreasing speed rate.
@@ -98,14 +80,41 @@ class LinearRegression:
             latest_error = error
             iteration += 1
 
+    def show_info(self, show_predicted_vs_output=False):
+        predicted_vs_putput = ""
+        if show_predicted_vs_output:
+            predicted_vs_putput = [f"{p} - {o}\n" for p, o in zip(self.predicted, self.output_vector)]
 
-class LogisticRegression(LinearRegression):
+        print(f"========== {self.__class__.__name__} ========= \n"
+              f"Count of Features: {self.features_count} | Len of Dataset: {self.len_dataset} | Epoch: {self.epoch} \n"
+              f"Weights: {self.weights}\n"
+              f"Accuracy: {self.accuracy}% | Error: {self.error}\n"
+              f"{''.join(predicted_vs_putput)}"
+              f"=====================================\n"
+              )
 
-    def normal_equation(self):
-        raise NotImplemented
+
+class LinearRegression(BaseRegression):
+
+    @property
+    def error(self):
+        # Calculate squared error of training set. Scalar value.
+        return (1 / (self.len_dataset * 2)) * np.power(self.predicted - self.output_vector, 2).sum()
 
     def hypothesis(self, vector):
-        # Calculate value of input vector based on current weight. Sigmoid activation.
+        # Calculate value of input vector based on current weight
+        return np.dot(self.weights, np.concatenate([np.ones(1), vector])).sum()
+
+    def normal_equation(self):
+        # Normal Equation method
+        theta = np.linalg.inv(self.trans_input.dot(self.input_vectors)).dot(self.trans_input).dot(self.outputs)
+        self.weights = theta.flatten()
+
+
+class LogisticRegression(BaseRegression):
+
+    def hypothesis(self, vector):
+        # Sigmoid activation of calculated values of input vector based on current weight.
         doted = np.dot(self.weights, np.concatenate([np.ones(1), vector]))
         return 1.0 / (1 + np.exp(-doted))
 
